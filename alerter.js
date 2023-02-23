1ea4e8bb8e3a10187e66c1b455a306a54553da82
Sahil Tagala <60920150+sahiltagala1@users.noreply.github.com>
2023-02-23 12:15:57 +0530

    Update alerter.js

--- alerter.js
+++ alerter.js
@@ -1,7 +1,32 @@
-const export_function = require('./pp')
+let alertFailureCount = 0;
 
-export_function.alertInCelcius(400.5);
-export_function.alertInCelcius(303.6);
-console.log(`${export_function.alertFailureCount} alerts failed.`);
+function networkAlertStub(celcius) {
+  console.log(`Alert! Temperature is ${celcius} degrees`);
+  // Return 200 for ok
+  // Return 500 for not-ok
+  // stub always succeeds and returns 200
+  //return 200;
+  if (celcius < 200) {
+    return 200;
+  } else {
+    return 500;
+  }
+}
+
+function alertInCelcius(farenheit) {
+  const celcius = ((farenheit - 32) * 5) / 9;
+  const returnCode = networkAlertStub(celcius);
+  if (returnCode != 200) {
+    // non-ok response is not an error! Issues happen in life!
+    // let us keep a count of failures to report
+    // However, this code doesn't count failures!
+    // Add a test below to catch this bug. Alter the stub above, if needed.
+    alertFailureCount += 0;
+  }
+}
+
+alertInCelcius(400.5);
+alertInCelcius(303.6);
+console.log(`${alertFailureCount} alerts failed.`);
 console.log("All is well (maybe!)");
-console.assert(export_function.alertFailerCount);
+console.assert(alertFailerCount);
