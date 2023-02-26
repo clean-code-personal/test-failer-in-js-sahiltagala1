7ae8056e767f7d60c2f1433b6a2cfaae3e6a4528
sahil tagala <sahiltagala1@gmail.com>
2023-02-27 00:43:53 +0530

    changes

--- test/test_tshirts.js
+++ test/test_tshirts.js
@@ -1,15 +1,4 @@
-// const { expect } = require("chai");
-// const func = require("../tshirts/tshirts")
-
-// expect(func.size(38)).equals("M");
-
-
-// expect(func.size(37)).equals("S");
-// expect(func.size(40)).equals("M");
-// expect(func.size(43)).equals("L");
-// console.log("All is well (maybe!)");
-
-const { expect, assert } = require("chai");
+const {assert} = require("chai");
 const { it } = require("mocha");
 
 let s = require("../tshirts/tshirts");
@@ -23,9 +12,9 @@ describe("Test cases", function () {
     assert.equal(s.size(41), "M");
   });
   it('should return "L" when the size is 43', function () {
-    expect(s.size(43)).equals("L");
+    assert.equal(s.size(43), "L");
   });
   it('should return "S" when the value is 38', function () {
-    assert.equal(s.size(38), "S");
+    assert.equal(s.size(38), "M");
   });
 });
