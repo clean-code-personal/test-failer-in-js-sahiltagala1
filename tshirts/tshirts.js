d7d56642f04906d0f9e02eaca8e807853d08cd7f
Sahil Tagala <60920150+sahiltagala1@users.noreply.github.com>
2023-02-26 10:28:09 +0530

    Update tshirts.js

--- tshirts/tshirts.js
+++ tshirts/tshirts.js
@@ -1,5 +1,3 @@
-const {expect} = require('chai');
-
 function size(cms) {
     if (cms < 38) {
         return 'S';
@@ -10,10 +8,9 @@ function size(cms) {
     }
 }
 
-expect(size(38)).equals('M');
-
-
-expect(size(37)).equals('S');
-expect(size(40)).equals('M');
-expect(size(43)).equals('L');
-console.log('All is well (maybe!)');
+module.exports = new export_file_tshirt();
+function export_file_tshirt(){
+    return{
+        size:size
+    }
+}
