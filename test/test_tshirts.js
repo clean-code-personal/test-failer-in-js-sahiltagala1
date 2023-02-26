1140c12e360e8172dd66f8298a8c6a15cf416d3c
sahil tagala <sahiltagala1@gmail.com>
2023-02-27 00:44:54 +0530

    changes

--- test/test_tshirts.js
+++ test/test_tshirts.js
@@ -1,20 +1,19 @@
 const {assert} = require("chai");
 const { it } = require("mocha");
 
-let s = require("../tshirts/tshirts");
+let func = require("../tshirts/tshirts");
 
-//Test cases using Mocha
 describe("Test cases", function () {
   it('should return "S" when the value is 37', function () {
-    assert.equal(s.size(37), "S");
+    assert.equal(func.size(37), "S");
   });
   it('should return "M" when the size is 41', function () {
-    assert.equal(s.size(41), "M");
+    assert.equal(func.size(41), "M");
   });
   it('should return "L" when the size is 43', function () {
-    assert.equal(s.size(43), "L");
+    assert.equal(func.size(43), "L");
   });
   it('should return "S" when the value is 38', function () {
-    assert.equal(s.size(38), "M");
+    assert.equal(func.size(38), "M");
   });
 });
