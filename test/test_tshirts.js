e02bf5497616e5b9cd73bedc576aa749355a8994
Sahil Tagala <60920150+sahiltagala1@users.noreply.github.com>
2023-02-27 09:42:56 +0530

    Update test_tshirts.js

--- test/test_tshirts.js
+++ test/test_tshirts.js
@@ -1,19 +1,19 @@
 const {assert} = require("chai");
 const { it } = require("mocha");
 
-let func = require("../tshirts/tshirts");
+let func_size = require("../tshirts/tshirts");
 
 describe("Test cases", function () {
-  it('should return "S" when the value is 37', function () {
-    assert.equal(func.size(37), "S");
+  it('return "S" for 37', function () {
+    assert.equal(func_size.size(37), "S");
   });
-  it('should return "M" when the size is 41', function () {
-    assert.equal(func.size(41), "M");
+  it('return "M" for 41', function () {
+    assert.equal(func_size.size(41), "M");
   });
-  it('should return "L" when the size is 43', function () {
-    assert.equal(func.size(43), "L");
+  it('return "L" for 43', function () {
+    assert.equal(func_size.size(43), "L");
   });
-  it('should return "S" when the value is 38', function () {
-    assert.equal(func.size(38), "M");
+  it('return "M" for 38', function () {
+    assert.equal(func_size.size(38), "M");
   });
 });
