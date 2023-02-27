e85a5c741364ce621aac1fb7d6991ca1db8d05ae
sahil tagala <sahiltagala1@gmail.com>
2023-02-27 19:03:39 +0530

    update

--- test/test_alerter.js
+++ test/test_alerter.js
@@ -2,8 +2,8 @@ const{assert} = require("chai");
 const { it } = require("mocha");
 const func = require("../alerter/alerter");
 
-func.alertFailerCount = func.alertInCelcius(400.5);
-func.alertFailerCount = func.alertInCelcius(303.6);
+func.alertInCelcius(400.5);
+func.alertInCelcius(303.6);
 
 describe("Test cases", function () {
   it("alert failure count", function () {
