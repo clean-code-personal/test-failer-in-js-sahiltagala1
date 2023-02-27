b70495092cca68a4ad64d1b6224423a6301eafd0
Sahil Tagala <60920150+sahiltagala1@users.noreply.github.com>
2023-02-27 18:46:19 +0530

    Update test_alerter.js

--- test/test_alerter.js
+++ test/test_alerter.js
@@ -1,10 +1,9 @@
 const{assert} = require("chai");
 const { it } = require("mocha");
-const alertFailerCount = require("../alerter/alerter");
 const func = require("../alerter/alerter");
 
-alertFailerCount = func.alertInCelcius(400.5);
-alertFailerCount = func.alertInCelcius(303.6);
+func.alertFailerCount = func.alertInCelcius(400.5);
+func.alertFailerCount = func.alertInCelcius(303.6);
 
 describe("Test cases", function () {
   it("alert failure count", function () {
