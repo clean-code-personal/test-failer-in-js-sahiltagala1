9ba8ca0aadc9f5490e03bc27b0c0bc666266186c
sahil tagala <sahiltagala1@gmail.com>
2023-02-27 09:50:09 +0530

    update

--- test/test_misaligned.js
+++ test/test_misaligned.js
@@ -3,7 +3,7 @@ const func = require("../misaligned/misaligned");
 
 result = func.print_color_map();
 
-// expect(result).equals(5);
+expect(result).equals(5);
 
 expect(result).equals(25);
 console.log("All is well (maybe!)");
